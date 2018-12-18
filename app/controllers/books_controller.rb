9202356ab7f0c0caf1e77e06f92ecd03a53156fd
John Mark Redding <johomurk@gmail.com>
2018-12-18 18:15:45 -0500

    minor changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,5 +4,8 @@ class BooksController < ApplicationController
     end
     def show
         @book = Book.find(params[:id])
+        @authors = Author.all
+    end
+    def new
     end
 end
--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,12 @@
 class ContributionsController < ApplicationController
+  def create
+    @contribution = Contribution.create(contribution_params)
+    redirect_to book_path(@contribution.book_id)
+  end
+
+  private
+
+  def contribution_params
+    params.require(:contribution).permit(:author_id, :book_id)
+  end
 end
