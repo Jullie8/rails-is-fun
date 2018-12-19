ead8b887ba6fe02ae9d2e74d7229671c4a4040c4
github username <github email address>
2018-12-18 19:37:39 -0500

    add remove author feature from contribution

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,9 +1,14 @@
 class ContributionsController < ApplicationController
   def create
-    @contribution = Contribution.create(contribution_params)
+    @contribution = Contribution.find_or_create_by(contribution_params)
     redirect_to book_path(@contribution.book_id)
   end
-
+  def destroy
+    @contribution = Contribution.find(params[:id])
+    @book_id = @contribution.book_id
+    @contribution.destroy
+    redirect_to book_path(@book_id)
+  end
   private
 
   def contribution_params
